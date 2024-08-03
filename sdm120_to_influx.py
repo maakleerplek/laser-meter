feec5159e062cbda003d877f93a9e6ad2a1a2dc5
kjetteren <160370317+kjetteren@users.noreply.github.com>
2024-08-03 15:30:04 +0200

    Added MongoDB Atlas configuration

--- sdm120_to_influx.py
+++ sdm120_to_influx.py
@@ -2,6 +2,8 @@ import minimalmodbus
 import serial
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 import time
 
 # InfluxDB configurations
@@ -9,6 +11,9 @@ influxdb_url = "http://localhost:8086"  # Replace with your InfluxDB URL
 influxdb_token = "k58HrZGegHYF-D28Pr14U4l7fP3ODT1-vieSA9E9pWKXroKn0AW4OFnJLAfDGRBmcSW_-1c89GVkas3AoFlCMA=="  # Fetch the token from the environment
 influxdb_org = "maakleerplek vzw"  # Replace with your InfluxDB organization
 influxdb_bucket = "db_meters"  # Replace with your InfluxDB bucket
+
+# MongoDB configurations
+uri = "mongodb+srv://admin:iwSTa98kZyWDw6Ti@cluster0.gx8iiuk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # SDM120 Modbus configuration
 sdm120_address = 1  # Modbus address of your SDM120
@@ -18,6 +23,9 @@ sdm120_baudrate = 2400  # Default baud rate for SDM120
 # Create an instance of the InfluxDB client
 client = InfluxDBClient(url=influxdb_url, token=influxdb_token, org=influxdb_org)
 write_api = client.write_api(write_options=SYNCHRONOUS)
+
+# Create an instance of the MongoDB client
+clientMongo = MongoClient(uri, server_api=ServerApi('1'))
 
 # Create a Modbus instrument object
 instrument = minimalmodbus.Instrument(sdm120_port, sdm120_address)
@@ -69,6 +77,14 @@ def write_to_influxdb(voltage, current, power):
         )
     except Exception as e:
         print(f"Error writing to InfluxDB: {e}")
+
+# Function to write data to InfluxDB
+def write_to_mongodb():
+    try:
+        clientMongo.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
 
 
 # Main loop to read and write data
